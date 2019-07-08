c37132390b4ae640c06f2f32868b2fc8fea69a77
mudlee <contact@mudlee.hu>
2019-07-08 10:29:15 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  "resolve": {
+    "alias": {
+      "@": path.join(__dirname, '../prometheus-ui/src'),
+      "vue$": "vue/dist/vue.esm.js"
+    },
+  },
+  "externals": {
+    vue: 'Vue'
+  }
+}
