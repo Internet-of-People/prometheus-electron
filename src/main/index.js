6e4e6d8028bd560409369568620dc87ffb2ba81f
Sandor Muranyi <contact@mudlee.hu>
2019-10-07 07:44:05 +0200

    Users now well notified if the prometheusd quits

--- src/main/index.js
+++ src/main/index.js
@@ -1,18 +1,18 @@
 'use strict'
 
-import { app, BrowserWindow, screen } from 'electron'
-import * as path from 'path'
-import { format as formatUrl } from 'url'
-import { PrometheusDaemon } from 'prometheus-neon'
+import { app, BrowserWindow, dialog, screen } from 'electron';
+import * as path from 'path';
+import { format as formatUrl } from 'url';
+import { PrometheusDaemon } from 'prometheus-neon';
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
-let mainWindow, prometheusDaemon
+let mainWindow, prometheusDaemon;
 
 function createMainWindow() {
-  const mainScreen = screen.getPrimaryDisplay()
-  const iconLocation = path.join(__dirname, '/icon/Icon-512x512.png')
+  const mainScreen = screen.getPrimaryDisplay();
+  const iconLocation = path.join(__dirname, '/icon/Icon-512x512.png');
 
   const window = new BrowserWindow({
     webPreferences: {
@@ -24,61 +24,67 @@ function createMainWindow() {
     minHeight: 600,
     width: ( mainScreen.size.width / 3.0 ) * 2.0,
     height: ( mainScreen.size.height / 3.0 ) * 2.0,
-  })
+  });
 
   if (isDevelopment) {
-    window.webContents.openDevTools()
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+    window.webContents.openDevTools();
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
   }
   else {
     window.loadURL(formatUrl({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file',
       slashes: true
-    }))
+    }));
   }
 
   window.on('closed', () => {
-    prometheusDaemon.done()
-    prometheusDaemon = null
-    mainWindow = null
-  })
+    if(prometheusDaemon) {
+      prometheusDaemon.done();
+    }
+    prometheusDaemon = null;
+    mainWindow = null;
+  });
 
   window.webContents.on('devtools-opened', () => {
-    window.focus()
+    window.focus();
     setImmediate(() => {
-      window.focus()
-    })
-  })
+      window.focus();
+    });
+  });
 
-  return window
+  return window;
 }
 
 // quit application when all windows are closed
 app.on('window-all-closed', () => {
   // on macOS it is common for applications to stay open until the user explicitly quits
   //if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   //}
-})
+});
 
 app.on('activate', () => {
   // on macOS it is common to re-create a window even after all windows have been closed
   if (mainWindow === null) {
-    mainWindow = createMainWindow()
+    mainWindow = createMainWindow();
   }
-})
+});
 
 // create main BrowserWindow when electron is ready
 app.on('ready', () => {
   try {
-    console.log('Instatiating PrometheusDaemon...')
-    prometheusDaemon = new PrometheusDaemon()
-    console.log('PrometheusDaemon is running')
+    console.log('Instatiating PrometheusDaemon...');
+    prometheusDaemon = new PrometheusDaemon();
+    console.log('PrometheusDaemon is running');
   }
   catch(err) {
-    console.log('Error while instatiating PrometheusDaemon:')
-    console.log(err)
+    dialog.showErrorBox(
+      'Error while instatiating PrometheusDaemon',
+      `Is it possible that Prometheus is already running? Error message: ${err}`
+    );
+    //alert(`Error while instatiating PrometheusDaemon. Is it possible that Prometheus is already running? Error message: ${err}`);
+    app.quit(0);
   }
-  mainWindow = createMainWindow()
-})
+  mainWindow = createMainWindow();
+});
